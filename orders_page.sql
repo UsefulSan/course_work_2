258511a19434a12dff1834c14e73189d843bb4f6
Aleksandr N <usefulsan@yandex.ru>
2022-10-20 15:35:59 +0500

    added request orders_page

---
+++ orders_page.sql
@@ -0,0 +1,22 @@
+-- Выбрать все заказы, отсортировать по required_date (по убыванию) и отсортировать по дате отгрузке (по возрастанию)
+
+SELECT *
+FROM orders
+ORDER BY required_date DESC;
+
+SELECT *
+FROM orders
+ORDER BY shipped_date;
+
+-- Найти среднее значение дней уходящих на доставку с даты формирования заказа в USA
+
+SELECT AVG(required_date - order_date)
+FROM orders
+WHERE ship_country = 'USA';
+
+-- Найти сумму, на которую имеется товаров (количество * цену) причём таких,
+-- которые не сняты с продажи (см. на поле discontinued)
+
+SELECT (unit_price * units_in_stock)
+FROM products
+WHERE discontinued = 0;
