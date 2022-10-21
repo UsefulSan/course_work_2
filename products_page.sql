601b62bd6db863310db8ba20f23fc9a7358564a4
Aleksandr N <usefulsan@yandex.ru>
2022-10-21 16:34:52 +0500

    added the condition about active products to the query

--- products_page.sql
+++ products_page.sql
@@ -5,4 +5,5 @@ SELECT products.product_name, units_in_stock, contact_name, phone
 FROM products
 LEFT JOIN suppliers ON suppliers.id_suppliers = products.product_id
 LEFT JOIN categories ON categories.category_id = products.category_id
-WHERE (categories.category_name = 'Beverages' OR categories.category_name = 'Seafood') AND units_in_stock < 20;
+WHERE (categories.category_name = 'Beverages' OR categories.category_name = 'Seafood')
+  AND units_in_stock < 20 AND discontinued = 0;
